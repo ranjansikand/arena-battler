a77fd592b02096d8aaec8df058f68fc9254c64c5
Ranjan Sikand <ranjans1998@gmail.com>
2022-02-02 18:02:49 -0800

    Audio and UI

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -10,28 +10,72 @@ using UnityEngine.SceneManagement;
 public class TitleManager : MonoBehaviour
 {
     PlayerControls _playerInput;
-    void Awake()
+    AudioSource _audiosource;
+
+    [Header("Menus")]
+    [SerializeField] GameObject _titleMenu;
+    [SerializeField] GameObject _controlsMenu;
+
+    [Header("Audio")]
+    [SerializeField] Slider _volumeControl;
+    [SerializeField] AudioClip _confirmation, _back, _close;
+    [SerializeField] AudioSource _backgroundMusicSource;
+
+
+    bool _controlsMenuActive = false;
+    float _masterVolume;
+
+    private void Awake()
     {
+        _audiosource = GetComponent<AudioSource>();
         _playerInput = new PlayerControls();
-        
+
         _playerInput.General.Quit.performed += OnQuitPressed;
+
+        _titleMenu.SetActive(true);
+        _controlsMenu.SetActive(false);
+
+        _masterVolume = PlayerPrefs.GetFloat("MasterVolume", 0.5f);
+        _volumeControl.value = _masterVolume;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void OnQuitPressed(InputAction.CallbackContext context) {
+    private void OnQuitPressed(InputAction.CallbackContext context) {
         QuitGame();
     }   
 
     public void StartGame() {
+        _audiosource.PlayOneShot(_confirmation, _masterVolume);
+
+        Invoke(nameof(PlayGame), 0.5f);
+    }
+
+    public void Quit() {
+        _audiosource.PlayOneShot(_close, _masterVolume);
+
+        Invoke(nameof(QuitGame), 0.5f);
+    }
+
+    public void Controls() {
+        _controlsMenuActive = !_controlsMenuActive;
+
+        if (_controlsMenuActive) _audiosource.PlayOneShot(_confirmation, _masterVolume);
+        else _audiosource.PlayOneShot(_back, _masterVolume);
+
+        _controlsMenu.SetActive(_controlsMenuActive);
+        _titleMenu.SetActive(!_controlsMenuActive);
+    }
+
+    public void SetVolume() {
+        _masterVolume = _volumeControl.value;
+        _backgroundMusicSource.volume = _masterVolume;
+        PlayerPrefs.SetFloat("MasterVolume", _masterVolume);
+    }
+
+    private void PlayGame() {
         SceneManager.LoadScene(1);
     }
 
-    public void QuitGame() {
+    private void QuitGame() {
         Application.Quit();
     }
 }
